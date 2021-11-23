4c48681a52a2f83ef0cc6b9105ca05835d84f3ea
paradelee <852791765@qq.com>
2021-11-23 19:01:27 +0800

    更新11.22

--- models/demoNet.py
+++ models/demoNet.py
@@ -4,22 +4,180 @@ import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
-class OverlapPatchEmbed(nn.Module):
-    def __init__(self, img_size=(224, 224), patch_size=7, stride=4, in_ch=3, embed_dim=768):
-        super(OverlapPatchEmbed, self).__init__()
-        patch_size = (patch_size, patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.proj = nn.Conv2d(in_ch, embed_dim, kernel_size=patch_size, stride=stride,
-                              # padding=(patch_size[0] // 2, patch_size[1] // 2)
-                              )
-        self.norm = nn.LayerNorm(embed_dim)
+class unetConv2(nn.Module):
+    def __init__(self, in_size, out_size, is_batchnorm):
+        super(unetConv2, self).__init__()
 
+        if is_batchnorm:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(in_size, out_size, 3, 1, 1), nn.BatchNorm2d(out_size), nn.ReLU()
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(out_size, out_size, 3, 1, 1), nn.BatchNorm2d(out_size), nn.ReLU()
+            )
+        else:
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1), nn.ReLU())
+            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1), nn.ReLU())
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size, is_deconv):
+        super(unetUp, self).__init__()
+        self.conv = unetConv2(in_size, out_size, False)
+        if is_deconv:
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
+        else:
+            self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+    def forward(self, inputs1, inputs2):
+        outputs2 = self.up(inputs2)
+        outputs1 = inputs1
+        outputs2 = F.interpolate(outputs2, size=[outputs1.size(2), outputs1.size(3)], mode='bilinear',
+                                 align_corners=True)
+
+        return self.conv(torch.cat([outputs1, outputs2], 1))
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, eps=1e-5):
+        super(LayerNorm, self).__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
 
     def forward(self, x):
-        # padding
-        _, _, H, W = x.size()
-        if W % self.patch_size[1] != 0:
-            x = F.pad(x, (0, self.patch_size[1] - W % self.patch_size[1]))
-        if H % self.patch_size[0] != 0:
-            x = F.pad(x, (0, 0, 0, self.patch_size[0] - H % self.patch_size[0]))
+        std = torch.var(x, dim=1, unbiased=False, keepdim=True).sqrt()
+        mean = torch.mean(x, dim=1, keepdim=True)
+        return (x-mean) / (std + self.eps) * self.g + self.b
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = LayerNorm(dim)
+    def forward(self, x):
+        return self.fn(self.norm(x))
+
+class UNetRNN(nn.Module):
+    def __init__(self, input_channel, n_classes, kernel_size, feature_scale=4, decoder="LSTM", bias=True):
+
+        super(UNetRNN, self).__init__()
+        self.input_channel = input_channel
+        self.n_classes = n_classes
+        self.kernel_size = kernel_size
+        self.feature_scale = feature_scale
+        self.decoder = decoder
+        self.bias = bias
+
+        filters = [64, 128, 256, 512, 1024]
+        filters = [int(x / self.feature_scale) for x in filters]
+
+        # downsampling
+        self.conv1 = unetConv2(self.input_channel, filters[0], is_batchnorm=True)
+
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = unetConv2(filters[0], filters[1], is_batchnorm=True)
+
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        self.conv3 = unetConv2(filters[1], filters[2], is_batchnorm=True)
+
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        self.conv4 = unetConv2(filters[2], filters[3], is_batchnorm=True)
+
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2)
+        self.center = unetConv2(filters[3], filters[4], is_batchnorm=True)
+
+        self.score_block1 = nn.Sequential(
+
+            nn.Conv2d(filters[0], self.n_classes, 5, padding=2),
+            nn.BatchNorm2d(self.n_classes),
+            nn.ReLU(inplace=True)
+        )
+
+        self.score_block2 = nn.Sequential(
+            nn.Conv2d(filters[1], self.n_classes, 5, padding=2),
+            nn.BatchNorm2d(self.n_classes),
+            nn.ReLU(inplace=True)
+        )
+
+        self.score_block3 = nn.Sequential(
+            nn.Conv2d(filters[2], self.n_classes, 5, padding=2),
+            nn.BatchNorm2d(self.n_classes),
+            nn.ReLU(inplace=True)
+        )
+
+        self.score_block4 = nn.Sequential(
+            nn.Conv2d(filters[3], self.n_classes, 5, padding=2),
+            nn.BatchNorm2d(self.n_classes),
+            nn.ReLU(inplace=True)
+        )
+
+        self.score_block5 = nn.Sequential(
+            nn.Conv2d(filters[4], self.n_classes, 5, padding=2),
+            nn.BatchNorm2d(self.n_classes),
+            nn.ReLU(inplace=True)
+        )
+
+        # self.RDC = RDC(self.n_classes, self.kernel_size, bias=self.bias, decoder=self.decoder)
+
+    def forward(self, input, cell_state=None):
+        conv1 = self.conv1(input)  # 1,filters[0]
+
+        maxpool1 = self.maxpool1(conv1)
+        conv2 = self.conv2(maxpool1)  # 1/2,filters[1]
+
+        maxpool2 = self.maxpool2(conv2)
+        conv3 = self.conv3(maxpool2)  # 1/4,filters[2]
+
+        maxpool3 = self.maxpool3(conv3)
+        conv4 = self.conv4(maxpool3)  # 1/8,filters[3]
+
+        maxpool4 = self.maxpool4(conv4)
+        conv5 = self.center(maxpool4)  # 1/16,filters[4]
+
+        x1 = self.score_block5(conv5)  # 1/16,class
+        x2 = self.score_block4(conv4)  # 1/8,class
+        x3 = self.score_block3(conv3)  # 1/4,class
+        x4 = self.score_block2(conv2)  # 1/2,class
+        x5 = self.score_block1(conv1)  # 1,class
+
+        h0 = self._init_cell_state(x1)  # 1/16,512
+
+        # Decode
+        if self.decoder == "LSTM":
+            # init c0
+            if cell_state is not None:
+                raise NotImplementedError()
+            else:
+                c0 = self._init_cell_state(h0)
+
+            h1, c1 = self.RDC(x_cur=x1, h_pre=h0, c_pre=c0)  # 1/16,class
+            h2, c2 = self.RDC(x_cur=x2, h_pre=h1, c_pre=c1)  # 1/8,class
+            h3, c3 = self.RDC(x_cur=x3, h_pre=h2, c_pre=c2)  # 1/4,class
+            h4, c4 = self.RDC(x_cur=x4, h_pre=h3, c_pre=c3)  # 1/2,class
+            h5, c5 = self.RDC(x_cur=x5, h_pre=h4, c_pre=c4)  # 1,class
+
+
+        elif self.decoder == "GRU":
+            h1 = self.RDC(x_cur=x1, h_pre=h0)  # 1/16,class
+            h2 = self.RDC(x_cur=x2, h_pre=h1)  # 1/8,class
+            h3 = self.RDC(x_cur=x3, h_pre=h2)  # 1/4,class
+            h4 = self.RDC(x_cur=x4, h_pre=h3)  # 1/2,class
+            h5 = self.RDC(x_cur=x5, h_pre=h4)  # 1,class
+
+        elif self.decoder == "vanilla":
+            h1 = self.RDC(x_cur=x1, h_pre=h0)  # 1/16,class
+            h2 = self.RDC(x_cur=x2, h_pre=h1)  # 1/8,class
+            h3 = self.RDC(x_cur=x3, h_pre=h2)  # 1/4,class
+            h4 = self.RDC(x_cur=x4, h_pre=h3)  # 1/2,class
+            h5 = self.RDC(x_cur=x5, h_pre=h4)  # 1,class
+
+        else:
+            raise NotImplementedError
+
+        return h5
+
