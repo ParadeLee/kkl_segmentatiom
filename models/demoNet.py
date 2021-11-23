e9febfe93db5e3187d7592fb8a921f38dfd2ff13
paradelee <852791765@qq.com>
2021-11-23 19:45:12 +0800

    更新11.23

--- models/demoNet.py
+++ models/demoNet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch import einsum
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
@@ -61,6 +62,56 @@ class PreNorm(nn.Module):
         self.norm = LayerNorm(dim)
     def forward(self, x):
         return self.fn(self.norm(x))
+
+class DsConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, kernel_size, padding, stride=1, bias=True):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(dim_in, dim_in, kernel_size=kernel_size, padding=padding, groups=dim_in, stride=stride, bias=bias),
+            nn.Conv2d(dim_in, dim_out, kernel_size=1, bias=bias)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class EfficientSelfAttention(nn.Module):
+    def __init__(self, *, dim, heads, reduction_ratio):
+        super().__init__()
+        self.scale = (dim // heads) ** -0.5
+        self.heads = heads
+
+        self.to_q = nn.Conv2d(dim, dim, 1, bias=False)
+        self.to_inputkv = nn.Conv2d(dim, dim*2, 1, bias=False)
+        self.to_kv = nn.Conv2d(dim, dim * 2, reduction_ratio, stride=reduction_ratio, bias=False)
+        self.to_out = nn.Conv2d(dim, dim, 1, bias=False)
+
+    def forward(self, x, s=None):
+        h, w = x.shape[-2:]
+        heads = self.heads
+        if s == None:
+            q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim=1))
+        else:
+            q, k, v = (self.to_q(x), *self.to_inputkv(s).chunk(2, dim=1))
+        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> (b h) (x y) c', h=heads), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+        attn = sim.softmax(dim=-1)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) (x y) c -> b (h c) x y', h=heads, x=h, y=w)
+        return self.to_out(out)
+
+class MixFeedForward(nn.Module):
+    def __init__(self, *, dim, expansion_factor):
+        super().__init__()
+        hidden_dim = dim * expansion_factor
+        self.net = nn.Sequential(
+            nn.Conv2d(dim, hidden_dim, 1),
+            DsConv2d(hidden_dim, hidden_dim, 3, padding=1),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, dim, 1)
+        )
+    def forward(self, x):
+        return self.net(x)
 
 class UNetRNN(nn.Module):
     def __init__(self, input_channel, n_classes, kernel_size, feature_scale=4, decoder="LSTM", bias=True):
